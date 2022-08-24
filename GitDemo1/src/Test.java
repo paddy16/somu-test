1dfbc37034c15b8c39b89d70cc6d18f16336c971
paddu <paddu.1616@gmail.com>
2022-08-24 11:25:46 +0530

    New project created and commited to local repo master branch.

---
+++ GitDemo1/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+	
+	public static void main(String args[]) {
+		
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes") {
+			System.out.println("Let's eat something");
+		}else {
+			System.out.println("DO your work!!!");
+		}
+		
+	}
+
+}
